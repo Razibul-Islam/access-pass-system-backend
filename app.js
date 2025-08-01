065f539424ee0e67ef89c7425db9bbe08cb8863b
Razibul-Islam <razibulislam665@gmail.com>
2025-08-01 22:46:33 +0600

    update mongodb local to server

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const eventRoutes = require("./routes/eventRoutes");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/events", eventRoutes);
 
-// Connect to MongoDB  mongodb+srv://<db_username>:<db_password>@cluster0.5xecffb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.5xecffb.mongodb.net/ipfs-events?retryWrites=true&w=majority&appName=Cluster0`,
+  `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.5xecffb.mongodb.net/ipfs-events`,
   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    tls: true,
+    tlsAllowInvalidCertificates: false,
+    tlsAllowInvalidHostnames: false,
   }
 );
 
