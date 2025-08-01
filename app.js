b31021497c089f87ffa6a23f26089e77a569686d
Razibul-Islam <razibulislam665@gmail.com>
2025-08-01 18:54:10 +0600

    updated local to server

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/events", eventRoutes);
 
-// Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/ipfs-events", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connect to MongoDB  mongodb+srv://<db_username>:<db_password>@cluster0.5xecffb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.5xecffb.mongodb.net/ipfs-events?retryWrites=true&w=majority&appName=Cluster0`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = app;
